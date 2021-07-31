334e730d3015df3bd97f0e6b722e5eeb27af12d1
Yerassyl <tleugazy98@gmail.com>
2021-07-31 15:04:14 +0600

    sdsd

---
+++ lesson5/10.py
@@ -0,0 +1,5 @@
+arr = [10, 31, 23, 34, 123, 34, 332, 32, 2323, 423, 231, ]
+print(arr)
+for i in range(len(arr)):
+    print(arr[i] - 20)
+print(arr)
---
+++ lesson5/9.py
@@ -0,0 +1,6 @@
+arr = [10, 31, 23, 34, 123, 34, 332, 32, 2323, 423, 231, ]
+n = len(arr)
+for i in range(n):
+    print(i, arr[i])
+# for i in arr:
+#     print(i)
