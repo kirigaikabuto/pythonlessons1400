c21246711c6a6a64ddb2207741786950a7f849c0
Yerassyl Tleugazy <tleugazy98@gmail.com>
2021-08-14 13:31:43 +0600

    sdsd

--- lesson7'/2.py
+++ lesson7'/2.py
@@ -1,2 +1,6 @@
-s = "Japans"
-print(s.istitle())
+s = "1234"
+num = 0
+if s.isnumeric():
+    num = int(s)
+else:
+    print("please write only numbers")
---
+++ lesson7'/3.py
@@ -0,0 +1,4 @@
+s = "Hello my name is Yerassyl, i am 23"
+print(s)
+s = s.replace(",", "")
+print(s)
